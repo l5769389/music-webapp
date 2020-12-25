8b0a1f3639a2d2ecce0008177bc802d17ccbe44b
appuser2 <ular1234@126.com>
2020-12-25 20:38:24 +0800

    初始化
    还没有代码，切换打dev开发

---
+++ src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<template >
+  <div>1</div>
+</template>
+
+<style scoped>
+
+</style>
+<script lang="ts">
+import {  defineComponent  } from 'vue'; 
+export default defineComponent({
+   name: 'index',
+});
+</script>
